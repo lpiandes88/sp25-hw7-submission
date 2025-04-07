c89dd5e04e4616ecd1703e8b6bbc7f0cf59eb96f
Benz Tran <bq.tran@assumption.edu>
2025-04-07 03:35:09 -0400

    add fibonacci program

---
+++ fibonacci.cpp
@@ -0,0 +1,6 @@
+// Write a c++ prorgam that display N values of fibonacci series
+// Given that the fibonacci series is defined:
+// a[1] = a[1] = 1
+// a[n] = a[n-1] + a[n-1]
+// Here is example of 10 first fibonacci values: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+
